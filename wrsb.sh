dbe6e06616c978e5560d7685e74664ffaf06946d
akeda <akeda2@gmail.com>
2026-03-06 21:40:25 +0100

    refactor: replace watch with loop for continuous repo checks and builds
    - replaced `watch` command with a loop to continuously schedule repo checks and trigger builds
    - added `sleep 2400` inside the loop to wait for 2400 seconds before running the next check

--- wrsb.sh
+++ wrsb.sh
@@ -3,4 +3,8 @@
 # Use watch and pueue to schedule repo checks and trigger builds
 
 set -e
-watch -cwt -n 2400 "pueue add -g rsb rsb"
+#watch -cwt -n 2400 "pueue add -g rsb rsb"
+while true; do
+    pueue add -g rsb rsb
+    sleep 2400
+done
