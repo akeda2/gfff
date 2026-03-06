1140fe6ff84f3e8b3f5a84e59a46e7a0c69c961c
akeda <akeda2@gmail.com>
2026-03-06 21:03:53 +0100

    fix: update rsb.sh script to exit with code 22 when no updates are found
    - modified rsb.sh script to include `exit 22` when no updates are found after running `git pull --ff-only`
    - updated comment to reflect the new behavior

--- rsb.sh
+++ rsb.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # For rust repos
-# Fetch and pull if repo has updates, then build. Needs gfff (git fetch and forward).
+# Fetch and pull if repo has updates, then build. Needs gfff (git fetch and forward). Exits 22 if no updates are found.
 
 set -e
 
-gfff && { git pull --ff-only && cargo build --release ;} || { echo "Already up to date!"; false ;}
+gfff && { git pull --ff-only && cargo build --release ;} || { echo "Already up to date!"; exit 22 ;}
