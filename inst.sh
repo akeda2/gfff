1bbb66c74095864de54b7999dc490e6f9f528e02
akeda <akeda2@gmail.com>
2026-03-11 19:25:37 +0100

    feat: add yt-dlp build script and service
    - added `ytdlpb.sh` script for building and installing yt-dlp
    - created `wrsb.service` to manage the wrsb build process

--- inst.sh
+++ inst.sh
@@ -4,3 +4,4 @@ sudo install -m 755 rsb.sh /usr/local/bin/rsb && echo "rsb install SUCCESS!" || 
 sudo install -m 755 gfff.sh /usr/local/bin/gfff && echo "gfff install SUCCESS!" || echo "gfff install FAIL!"
 sudo install -m 755 wrsb.sh /usr/local/bin/wrsb && echo "wrsb install SUCCESS!" || echo "wrsb install FAIL!"
 sudo install -m 755 buildbot.py /usr/local/bin/gfff-buildbot && echo "gfff-buildbot install SUCCESS!" || echo "gfff-buildbot install FAIL!"
+sudo install -m 755 ytdlpb.sh /usr/local/bin/ytdlpb && echo "ytdlpb install SUCCESS!" || echo "ytdlpb install FAIL!"
