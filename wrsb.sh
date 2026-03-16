7483c2c186ffeb5fce27ce13f4a8c9b57961c8c0
akeda <akeda2@gmail.com>
2026-03-16 09:40:41 +0100

    chore: comment out set -e in wrsb.sh
    - commented out `set -e` to disable script exit on error

--- wrsb.sh
+++ wrsb.sh
@@ -2,7 +2,7 @@
 
 # Use watch and pueue to schedule repo checks and trigger builds
 SLEEP=3600
-set -e
+#set -e
 #watch -cwt -n $SLEEP "pueue add -g rsb rsb"
 while true; do
     pueue group add --parallel 1 rsb
